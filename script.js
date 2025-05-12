98b9e3131c9601e2172585b760a19be8b41df558
noey-na <noeyna@naver.com>
2025-05-12 17:09:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,4 +10,18 @@ document.addEventListener(`DOMContentLoaded`, function () {
   closeBtn.addEventListener(`click`, () => {
     subMenu.classList.remove(`show`);
   });
+
+  // 마우스휠 헤더 이벤트
+  // scrollEvent 클래스 추가 제거로 조정
+  window.addEventListener(`wheel`, (event) => {
+    const headerArea = document.querySelector(`.header_area`);
+
+    if (event.deltaY > 0) {
+      // wheel down
+      headerArea.classList.remove(`scrollEvent`);
+    } else {
+      // wheel up
+      headerArea.classList.add(`scrollEvent`);
+    }
+  });
 });
